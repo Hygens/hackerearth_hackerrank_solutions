5cd43b431f7788960bc655ad32497176360adef8
Josenildo Silva <hygenscolt@yahoo.com>
2016-03-31 12:38:44 -0300

    Added files via upload

--- str_concat.cpp
+++ str_concat.cpp
@@ -12,13 +12,13 @@ string trim(string& str)
   return str;
 }
 
-//int main() {
-//   string a,b;
-//    cin>>a; cin>>b; a= trim(a); b= trim(b);
-//    transform(a.begin(), a.end(), a.begin(), ::tolower);
-//    transform(b.begin(), b.end(), b.begin(), ::tolower);
-//    cout<<a.size()<<" "<<b.size()<<"\n";
-//    cout<<a+b<<"\n";
-//    cout<<b.substr(0,1)+a.substr(1,a.size())<<" "<<a.substr(0,1)+b.substr(1,b.size())<<"\n";
-//    return 0;
-//}
+int main() {
+   string a,b;
+    cin>>a; cin>>b; a= trim(a); b= trim(b);
+    transform(a.begin(), a.end(), a.begin(), ::tolower);
+    transform(b.begin(), b.end(), b.begin(), ::tolower);
+    cout<<a.size()<<" "<<b.size()<<"\n";
+    cout<<a+b<<"\n";
+    cout<<b.substr(0,1)+a.substr(1,a.size())<<" "<<a.substr(0,1)+b.substr(1,b.size())<<"\n";
+    return 0;
+}
