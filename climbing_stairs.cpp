75cb03b0acf53f2cd365a4c873134be2075a37cd
Josenildo Silva <hygenscolt@yahoo.com>
2016-04-02 19:34:19 -0300

    Added files via upload

--- climbing_stairs.cpp
+++ climbing_stairs.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-	long A,B,N;
-	int count;
-	cin >> A >> B >> N;
-	while (1)
-	{
-		N=N-A;
-		count++;
-		if(N<=0) break;
-		N=N+B;
-	}
-	cout<<count<<endl;
-	return 0;
-}
+//int main()
+//{
+//	long A,B,N;
+//	int count;
+//	cin >> A >> B >> N;
+//	while (1)
+//	{
+//		N=N-A;
+//		count++;
+//		if(N<=0) break;
+//		N=N+B;
+//	}
+//	cout<<count<<endl;
+//	return 0;
+//}
