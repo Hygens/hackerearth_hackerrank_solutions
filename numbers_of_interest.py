4264ee84ccb0451f9b4dcbbedf9436ebc497443c
Josenildo Silva <hygenscolt@yahoo.com>
2016-04-02 13:08:53 -0300

    Added files via upload

---
+++ numbers_of_interest.py
@@ -0,0 +1,11 @@
+T=int(raw_input())
+for n in range (T):
+    X,Y,N=map(int,raw_input().split(" ")) ;  L=[Y for i in range(X)]
+    if N<=X: print Y
+    else: 
+        for i in range(X+1,N+1):
+            if i==X+1:
+                L.append(sum(L))
+            else:
+                L.append(sum(L[i-(X+1):i]))
+        print(L[N-1])
