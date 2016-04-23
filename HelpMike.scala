914a17eb50c6dffee152c9ceb43bb424696c2c25
Josenildo Silva <hygenscolt@yahoo.com>
2016-04-23 20:13:54 -0300

    Added files via upload

---
+++ HelpMike.scala
@@ -0,0 +1,22 @@
+package com.scala.examples
+
+object HelpMike {  
+  var K=0
+  def calc(i: Int,j: Int) : Int = (i,j) match {
+    case (i,j) if (i < j && (i + j) % K == 0) =>  1
+    case _ => 0
+  }
+  def main(args: Array[String]): Unit = {
+    var T = readInt()
+    var count=0
+    while (T > 0) {
+      val Array(n, k) = readLine().split(" ").map(_.toInt)
+      K=k
+      count+=(1 to n).combinations(2).toList.par.filter{
+        case IndexedSeq(i,j) => (i < j && (i + j) % k == 0)}.size
+      println(count)
+      count=0
+      T -= 1      
+    }
+  }
+}
