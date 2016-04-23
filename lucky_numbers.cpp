cb39bf75f0c4f9a718b5125c3b3d0b26bf530afc
Josenildo Silva <hygenscolt@yahoo.com>
2016-04-23 20:45:41 -0300

    Added files via upload

---
+++ lucky_numbers.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iostream>
+#include <bitset>
+using namespace std;
+int main() {
+    long int T;
+    cin>>T;
+    while(T--){
+    	long int N=0; int S=0; string binary; int occur;
+        cin>>N; ++N;
+        for(long int i=1;i<N;i++) {
+        	binary=bitset<64>(i).to_string();
+        	occur=count(binary.begin(), binary.end(), '1');
+            if (occur==2) S+=i;
+        }
+        cout<<S<<endl;
+    }
+    return 0;
+}
