c234c8f947684bcff1fae05c08a04d607391c3fd
Josenildo Silva <hygenscolt@yahoo.com>
2016-05-12 14:28:34 -0300

    Update NumberOfDivisorsInRange.py

--- NumberOfDivisorsInRange.py
+++ NumberOfDivisorsInRange.py
@@ -5,9 +5,3 @@ while T>0:
     print(r)
     T-=1
     
-# print("Levou: "+str(decimal.Decimal(timeit.default_timer()-before)))
-#     r= len(filter(lambda x: x%M==0,range(A,B)))
-#     before=timeit.default_timer()
-#     r= len(filter(lambda x: x%M==0,range(A,B)))
-# import timeit
-# import decimal
